ea862e24dd6432a680511fdf7a47151539e6fe50
Eugene <ztarasenko@gmail.com>
2021-07-26 17:25:54 +0300

    Healthchecks added

---
+++ Pokedex/Pokedex.WebAPI/HealthChecks/PokemonStoreHealthCheck.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Pokedex.WebAPI.Entities;
+using Pokedex.WebAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pokedex.WebAPI.HealthChecks
+{
+    public class PokemonStoreHealthCheck : IHealthCheck
+    {
+        private readonly IStore<Pokemon> store;
+        public PokemonStoreHealthCheck(IStore<Pokemon> store)
+        {
+            this.store = store;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            Pokemon result;
+            try
+            {
+                result = await store.GetByNameAsync("mewtwo");
+            }
+            catch
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+            return result == null ? HealthCheckResult.Unhealthy() : HealthCheckResult.Healthy();
+        }
+    }
+}
--- Pokedex/Pokedex.WebAPI/Startup.cs
+++ Pokedex/Pokedex.WebAPI/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Pokedex.WebAPI.Entities;
+using Pokedex.WebAPI.HealthChecks;
 using Pokedex.WebAPI.Interfaces;
 using Pokedex.WebAPI.Services;
 using Pokedex.WebAPI.Translators;
@@ -27,7 +30,9 @@ namespace Pokedex.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHealthChecks()
+                .AddCheck("StartupCheck", () => HealthCheckResult.Healthy(), tags: new [] {"startup"})
+                .AddCheck<PokemonStoreHealthCheck>(nameof(PokemonStoreHealthCheck), tags: new[] { "liveness" });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -52,6 +57,9 @@ namespace Pokedex.WebAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseHealthChecks("/hc/startup", new HealthCheckOptions() { Predicate = x => x.Tags.Contains("startup") });
+            app.UseHealthChecks("/hc/live", new HealthCheckOptions() { Predicate = x => x.Tags.Contains("liveness") });
 
             app.UseRouting();
 
